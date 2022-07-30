274c34ceb03e130079e4c50da9beaf56438ef05a
JosephKnecht-lab <igorstrumica@gmail.com>
2022-07-30 11:38:09 +0100

    add functionality and tests

--- pallets/grant/src/lib.rs
+++ pallets/grant/src/lib.rs
@@ -47,6 +47,7 @@
 
 pub use pallet::*;
 
+
 #[cfg(test)]
 mod mock;
 
@@ -63,10 +64,15 @@ pub mod pallet {
 	storage::bounded_vec::BoundedVec,
 	pallet_prelude::*};
 	use frame_system::pallet_prelude::*;
-	use frame_support::{ sp_runtime::traits::Hash,
-		traits::{Currency, tokens::ExistenceRequirement}};
+	use frame_support::{ 
+		sp_runtime::traits::{Hash, Zero},
+		traits::{
+			Currency, 
+			tokens::ExistenceRequirement,
+		}};
 	use scale_info::TypeInfo;
 	use crate::weights::WeightInfo;
+
 
 
 	// Account, Balance are used in Profile Struct
@@ -74,6 +80,14 @@ pub mod pallet {
 	type BalanceOf<T> =
 		<<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
 
+
+		// Struct for holding Profile information.
+	#[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo)]
+	#[scale_info(skip_type_params(T))]
+	pub struct Requesters<T: Config> {
+		pub owner: AccountOf<T>,
+		pub balance: Option<BalanceOf<T>>,
+	}
 
 	// Struct for holding Profile information.
 	#[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo)]
@@ -131,6 +145,12 @@ pub mod pallet {
 	/// Stores a Profile unique properties in a StorageMap.
 	pub(super) type Profiles<T: Config> = StorageMap<_, Twox64Concat, T::AccountId, Profile<T>>;
 
+
+	#[pallet::storage]
+	#[pallet::getter(fn storage_requesters)]
+	/// Stores a Profile unique properties in a StorageMap.
+	pub(super) type StorageRequesters<T: Config> = StorageMap<_, Twox64Concat, T::AccountId, Requesters<T>>;
+
 	#[pallet::storage]
 	#[pallet::getter(fn completed_tasks)]
 	/// Stores list of completed tasks for a profile.
@@ -143,10 +163,10 @@ pub mod pallet {
 		GrantIssued { who: T::AccountId },
 
 		/// Profile was successfully deleted.
-		ProfileDeleted { who: T::AccountId },
+		GrantRequested { who: T::AccountId },
 
 		/// Profile was successfully updated.
-		ProfileUpdated { who: T::AccountId },
+		WinnerSelected { who: T::AccountId },
 
 		/// A task completed by profile
 		TaskCompletedByProfile { who: T::AccountId, task: T::Hash },
@@ -166,6 +186,14 @@ pub mod pallet {
 		NoProfileCreated,
 		/// Completed task storage reached its bound.
 		CompletedTasksStorageFull,
+		/// Cant grant to recieving account
+		CantGrantToSelf,
+		/// No further funds available
+		ZeroAmountAvailable,
+		// User has already made requests
+		RequestAlreadyMade,
+		// You must have empty balance to receive tokens.
+		NonEmptyBalance,
 	}
 
 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
@@ -174,202 +202,75 @@ pub mod pallet {
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 
-		/// Dispatchable call that enables every new actor to create personal profile in storage.
-		#[pallet::weight(<T as Config>::WeightInfo::create_profile(0,0))]
-		pub fn issue_grant(origin: OriginFor<T>, grant_receiver: T::AccountId) -> DispatchResult {
-
-			// Check that the extrinsic was signed and get the signer.
-			let account = ensure_signed(origin)?;
-
-			// Call helper function to generate Profile Struct
-			// let _profile_id = Self::generate_profile(&account, username, interests,
-			// 	available_hours_per_week, additional_information)?;
-
-            <T as self::Config>::Currency::transfer(&account, &grant_receiver, 1u32.into(), ExistenceRequirement::KeepAlive)?;
-
-			// // Emit an event.
-			Self::deposit_event(Event::GrantIssued{ who:account });
-
-			Ok(())
-		}
-
 		/// Dispatchable call that ensures user can update existing personal profile in storage.
 		#[pallet::weight(<T as Config>::WeightInfo::update_profile(0))]
-		pub fn update_profile(origin: OriginFor<T>, username: BoundedVec<u8, T::MaxUsernameLen>, interests: BoundedVec<u8, T::MaxInterestsLen>, available_hours_per_week: u8, 
-			additional_information : Option<BoundedVec<u8, T::MaxAdditionalInformationLen>>) -> DispatchResult {
+		pub fn request_grant(origin: OriginFor<T>, grant_requester: T::AccountId) -> DispatchResultWithPostInfo {
 
 			// Check that the extrinsic was signed and get the signer.
 			let account = ensure_signed(origin)?;
 
-			// Since Each account can have one profile, we call into generate profile again
-			let _profile_id = Self::change_profile(&account, username, interests,
-				available_hours_per_week, additional_information)?;
+			Self::generate_requests(&grant_requester);
+			
+			Self::deposit_event(Event::GrantRequested{ who:account });
 
-			// Emit an event.
-			Self::deposit_event(Event::ProfileUpdated{ who: account });
-
-			Ok(())
+			// pays no fees
+			Ok(Pays::No.into())
 		}
 
-		/// Dispatchable call that enables every new actor to delete profile from storage.
-		#[pallet::weight(<T as Config>::WeightInfo::remove_profile(0))]
-		pub fn remove_profile(origin: OriginFor<T>) -> DispatchResult {
+		#[pallet::weight(<T as Config>::WeightInfo::update_profile(0))]
+		pub fn winner_is(origin: OriginFor<T>) -> DispatchResultWithPostInfo {
 
 			// Check that the extrinsic was signed and get the signer.
 			let account = ensure_signed(origin)?;
 
-			// Call helper function to delete profile
-			Self::delete_profile(&account)?;
+			//Self::select_winner(&grant_requester);
+			
+			Self::deposit_event(Event::WinnerSelected{ who:account });
 
-			// Emit an event.
-			Self::deposit_event(Event::ProfileDeleted{ who : account});
-
-			Ok(())
+			// pays no fees
+			Ok(Pays::No.into())
 		}
 	}
 
 	// ** Helper internal functions ** //
 	impl<T:Config> Pallet<T> {
 		// Generates initial Profile.
-		pub fn generate_profile(owner: &T::AccountId, name: BoundedVec<u8, T::MaxUsernameLen>, interests: BoundedVec<u8, T::MaxInterestsLen>, available_hours_per_week: u8, additional_information: Option<BoundedVec<u8, T::MaxAdditionalInformationLen>>) -> Result<T::Hash, DispatchError> {
 
-			// Check if profile already exists for owner
-			ensure!(!Profiles::<T>::contains_key(&owner), Error::<T>::ProfileAlreadyCreated);
+		//let total_requests: u32 = 0;
+		pub fn generate_requests(grant_receiver: &T::AccountId) -> Result<T::Hash, DispatchError> {
 
 			// Get current balance of owner
-			let balance = T::Currency::free_balance(owner);
+			let balance = T::Currency::free_balance(grant_receiver);
+			// ensure!(balance > T::ExistentialDeposit::get() , Error::<T>::NonEmptyBalance);
 
-			// Populate Profile struct
-			let profile = Profile::<T> {
-				owner: owner.clone(),
-				name,
-				interests,
-				balance: Some(balance),
-				reputation: 0,
-				available_hours_per_week,
-				additional_information,
+			let total = T::Currency::total_issuance();
+		
+			// Populate Requesters struct
+			let mut requesters = Requesters::<T> {
+				owner: grant_receiver.clone(),
+				balance: Some(balance)
 			};
 
 			// Get hash of profile
-			let profile_id = T::Hashing::hash_of(&profile);
+			let requesters_id = T::Hashing::hash_of(&requesters);
 
 			// Insert profile into HashMap
-			<Profiles<T>>::insert(owner, profile);
-
-			// Initialize completed tasks list with default value.
-			<CompletedTasks<T>>::insert(owner, BoundedVec::default());
-
+			<StorageRequesters<T>>::insert(grant_receiver, requesters);
 
 			// Increase profile count
 			let new_count = Self::profile_count().checked_add(1).ok_or(<Error<T>>::ProfileCountOverflow)?;
 			<ProfileCount<T>>::put(new_count);
 
-			Ok(profile_id)
+			Ok(requesters_id)
 		}
 
-		// Changes existing profile
-		pub fn change_profile(owner: &T::AccountId, new_username: BoundedVec<u8, T::MaxUsernameLen>, new_interests: BoundedVec<u8, T::MaxInterestsLen>, new_available_hours_per_week: u8, new_additional_information: Option<BoundedVec<u8, T::MaxAdditionalInformationLen>>) -> Result<T::Hash, DispatchError> {
-
-			// Ensure that only owner can update profile
-			let mut profile = Self::profiles(owner).ok_or(<Error<T>>::NoProfileCreated)?;
-
-			// Change interests of owner
-			profile.change_interests(new_interests);
-
-			profile.change_username(new_username);
-
-			profile.change_available_hours_per_week(new_available_hours_per_week);
-
-			profile.change_additional_information(new_additional_information);
-			// Get hash of profile
-			let profile_id = T::Hashing::hash_of(&profile);
-
-			// Insert profile into HashMap
-			<Profiles<T>>::insert(owner, profile);
-
-			// Return hash of profileID
-			Ok(profile_id)
-		}
-
-		// Public function that deletes a user profile
-		pub fn delete_profile(owner: &T::AccountId) -> Result<(), DispatchError> {
-
-			// Ensure that only creator of profile can delete it
-			Self::profiles(owner).ok_or(<Error<T>>::NoProfileCreated)?;
-
-			// Remove profile from storage
-			<Profiles<T>>::remove(owner);
-
-			// Reduce profile count
-			let new_count = Self::profile_count().saturating_sub(1);
-			<ProfileCount<T>>::put(new_count);
-
-			Ok(())
-		}
-
-		// Public function that adds reputation to a profile
-		pub fn add_reputation(owner: &T::AccountId) -> Result<(), DispatchError> {
-
-			// Get current profile
-			let mut profile = Self::profiles(owner).ok_or(<Error<T>>::NoProfileCreated)?;
-
-			// Increase reputation
-			profile.increase_reputation();
-
-			// Insert into storage a new profile
-			<Profiles<T>>::insert(owner, profile);
-
-			Ok(())
-		}
-
-		// Public function that check if user has a profile
-		pub fn has_profile(owner: &T::AccountId) -> Result<bool, DispatchError>  {
+		// Public function that check if user has made requests
+		pub fn has_made_requests(owner: &T::AccountId) -> Result<bool, DispatchError>  {
 
 			// Check if an account has a profile
-			Self::profiles(owner).ok_or(<Error<T>>::NoProfileCreated)?;
+			Self::storage_requesters(owner).ok_or(<Error<T>>::RequestAlreadyMade)?;
 
 			Ok(true)
-		}
-
-		pub fn add_task_to_completed_tasks(owner: &T::AccountId, task: T::Hash) -> Result<(),
-		DispatchError> {
-			<CompletedTasks<T>>::mutate(owner, |completed_tasks| -> Result<(), DispatchError> {
-				if let Some(ct) = completed_tasks {
-				ct.try_push(task).map_err(|_|
-					// TODO: Instead of throwing an error, we have to clear up older history.
-					Error::<T>::CompletedTasksStorageFull.into())
-				} else {
-				Ok(())
-				}
-			})
-		}
-	}
-
-	// Change the reputation on a Profile (TODO MVP2: Improve reputation functions)
-	impl<T:Config> Profile<T> {
-		pub fn increase_reputation(&mut self) {
-			self.reputation += 1;
-		}
-
-		pub fn decrease_reputation(&mut self) {
-			self.reputation -= 1;
-		}
-
-		pub fn change_interests(&mut self, new_interests: BoundedVec<u8, T::MaxInterestsLen>) {
-			self.interests = new_interests;
-		}
-
-		pub fn change_username(&mut self, new_username: BoundedVec<u8, T::MaxUsernameLen>) {
-			self.name = new_username;
-		}
-
-		pub fn change_available_hours_per_week(&mut self, new_available_hours_per_week: u8) {
-			self.available_hours_per_week = new_available_hours_per_week;
-		}
-
-		pub fn change_additional_information(&mut self, new_additional_information: Option<BoundedVec<u8,
-			T::MaxAdditionalInformationLen>>) {
-			self.additional_information =  new_additional_information;
 		}
 	}
 
--- pallets/grant/src/tests.rs
+++ pallets/grant/src/tests.rs
@@ -4,22 +4,100 @@ use crate::{mock::*, Error};
 use frame_support::{assert_noop, assert_ok};
 
 
+// #[test]
+// fn issue_grant_to_account() {
+// 	new_test_ext().execute_with(|| {
+
+// 		// Ensure the user can create profile
+// 		assert_ok!(Grant::issue_grant(Origin::signed(1), 2 ));
+// 	});
+// }
+
+// #[test]
+// fn ensure_funds_are_transfered() {
+// 	new_test_ext().execute_with(|| {
+
+// 		// Ensure the user can create profile
+// 		assert_ok!(Grant::issue_grant(Origin::signed(1), 2 ));
+
+//         assert_eq!(Balances::free_balance(&2), 11);
+// 	});
+// }
+
 #[test]
-fn issue_grant_to_account() {
+fn accounts_can_request_a_grant() {
 	new_test_ext().execute_with(|| {
 
 		// Ensure the user can create profile
-		assert_ok!(Grant::issue_grant(Origin::signed(1), 2 ));
+		assert_ok!(Grant::request_grant(Origin::signed(1), 2 ));
+
+        
 	});
 }
 
 #[test]
-fn ensure_funds_are_transfered() {
+fn throw_error_when_granting_to_self() {
 	new_test_ext().execute_with(|| {
 
 		// Ensure the user can create profile
-		assert_ok!(Grant::issue_grant(Origin::signed(1), 2 ));
+		// assert_noop!(Grant::request_grant(Origin::signed(1), 1 , 3 ), Error::<Test>::CantGrantToSelf);
+	});
+}
 
-        // assert_eq!(Balances::balance(&2), 1000);
+#[test]
+fn ensure_request_is_stored() {
+	new_test_ext().execute_with(|| {
+
+		// Ensure a user can request a grant
+		assert_ok!(Grant::request_grant(Origin::signed(1), 1 ));
+
+        // Find the request
+        let requests = Grant::storage_requesters(1).expect("should find requests");
+
+        // Ensure we can access the storage requests
+        assert_eq!(requests.owner, 1);
+        assert_eq!(requests.balance,Some(10));
+
+	});
+}
+
+#[test]
+fn ensure_requests_can_be_made_by_separate_accounts() {
+	new_test_ext().execute_with(|| {
+
+		// Ensure a user can request a grant
+		assert_ok!(Grant::request_grant(Origin::signed(1), 1 ));
+        assert_ok!(Grant::request_grant(Origin::signed(1), 2 ));
+
+        // Find the request
+        let request1 = Grant::storage_requesters(1).expect("should find requests");
+        let request2 = Grant::storage_requesters(2).expect("should find requests");
+
+        // Ensure we can access the storage requests
+        assert_eq!(request1.owner, 1);
+        assert_eq!(request2.owner, 2);
+
+	});
+}
+
+#[test]
+fn ensure_only_users_with_no_balance_can_request_grants() {
+	new_test_ext().execute_with(|| {
+
+		// Ensure a user can request a grant
+		// assert_ok!(Grant::request_grant(Origin::signed(3), 3 ));
+        
+        // Ensure only empty balance can make requests
+        // assert_noop!(Grant::request_grant(Origin::signed(1), 3 ), Error::<Test>::NonEmptyBalance);
+
+	});
+}
+
+#[test]
+fn random_account_is_selected() {
+	new_test_ext().execute_with(|| {
+
+		
+
 	});
 }
