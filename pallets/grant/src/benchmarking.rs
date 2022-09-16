d0a5a18a71e366bfb74b69705b4cb73b33104a35
gatsey <felixgate13@gmail.com>
2022-09-16 15:09:19 +0100

    fixed benchmarks for pr

--- pallets/grant/src/benchmarking.rs
+++ pallets/grant/src/benchmarking.rs
@@ -24,7 +24,7 @@ use crate::Pallet as PalletGrant;
 use crate::Config as PalletConfig;
 use frame_benchmarking::{account, benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_system::RawOrigin;
-use frame_support::traits::Currency;
+use frame_support::traits::{Currency, Get};
 
 const SEED: u32 = 10; 
 
@@ -34,9 +34,15 @@ fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
 	frame_system::Pallet::<T>::assert_last_event(generic_event.into());
 }
 
-fn setup_account<T: Config>() -> (T::AccountId, BalanceOf<T>) {
+fn setup_account<T: Config>(is_empty: bool) -> (T::AccountId, BalanceOf<T>)
+where
+T: PalletConfig
+{
 	let account = account("account", SEED, SEED);
-	let value: BalanceOf<T> = 10_000_000u32.into();
+	let mut value: BalanceOf<T> = T::ExistentialDeposit::get().into();
+	if !is_empty {
+		value = 10_000_000u32.into();
+	}
 	let _ = T::Currency::make_free_balance_be(&account, value);
 	(account, value)	
 }
@@ -45,29 +51,27 @@ benchmarks! {
 	request_grant {
 		/* setup initial state */
 
-		let caller: T::AccountId = whitelisted_caller();
-		let grant_receiver:  T::AccountId = whitelisted_caller();
-	}: request_grant(RawOrigin::Signed(caller))
+		let (caller, _)= setup_account::<T>(true);
+	}: request_grant(RawOrigin::Signed(caller.clone()))
 
 	verify {
 		/* verifying final state */
-		let caller: T::AccountId = whitelisted_caller();
 		assert_last_event::<T>(Event::<T>::GrantRequested { who: caller }.into());
 	}
 
 	transfer_to_treasury {
-		let (account_id, value) = setup_account::<T>();
+		let (account_id, value) = setup_account::<T>(false);
 		
-	}: transfer_to_treasury(RawOrigin::Signed(account_id.clone()), 100u32.into())
+	}: transfer_to_treasury(RawOrigin::Signed(account_id.clone()), 1000u32.into())
 	verify {
 		assert_last_event::<T>(Event::<T>::TreasuryDonation{ who: account_id }.into());
 	}
 
 	 winner_is {
-		 let (account_id, value) = setup_account::<T>();
-		 let grant_receiver: T::AccountId = whitelisted_caller();
+		 let (account_id, _) = setup_account::<T>(false);
+		 let _ = PalletGrant::<T>::transfer_to_treasury(RawOrigin::Signed(account_id.clone()).into(), 1000u32.into())?;
 
-		 let _ = PalletGrant::<T>::transfer_to_treasury(RawOrigin::Signed(account_id.clone()).into(), 900u32.into())?;
+		 let (grant_receiver, _) = setup_account::<T>(true);
 		 <Winner<T>>::put(&grant_receiver);
 
 	 }: winner_is(RawOrigin::Signed(grant_receiver.clone()))
